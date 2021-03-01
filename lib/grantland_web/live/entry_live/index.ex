d42a7ea2499a3c2ef32236ff775f6a695e1075d3
Aristides Perez <aris.a.perez@gmail.com>
2021-02-28 21:05:45 -0500

    Add entries

---
+++ lib/grantland_web/live/entry_live/index.ex
@@ -0,0 +1,70 @@
+defmodule GrantlandWeb.EntryLive.Index do
+  use GrantlandWeb, :live_view
+
+  alias Grantland.Engine
+  alias Grantland.Engine.Entry
+
+  @impl true
+  def mount(_params, _session, socket) do
+    if connected?(socket), do: Engine.subscribe()
+    {:ok, assign(socket, :entries, list_entries()), temporary_assigns: [entries: []]}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    socket
+    |> assign(:page_title, "Edit Entry")
+    |> assign(:entry, Engine.get_entry!(id))
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "New Entry")
+    |> assign(:entry, %Entry{})
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Listing Entries")
+    |> assign(:entry, nil)
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    entry = Engine.get_entry!(id)
+
+    case Engine.delete_entry(entry) do
+      {:ok, _user} ->
+        {
+          :noreply,
+          assign(socket, :entries, list_entries())
+          |> put_flash(:info, "Entry deleted successfully")
+        }
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  @impl true
+  def handle_info({:entry_created, entry}, socket) do
+    {:noreply, update(socket, :entries, fn entries -> [entry | entries] end)}
+  end
+
+  def handle_info({:entry_deleted, entry}, socket) do
+    {:noreply, update(socket, :entries, fn entries -> [entry | entries] end)}
+  end
+
+  @impl true
+  def handle_info({:entry_updated, entry}, socket) do
+    {:noreply, update(socket, :entries, fn entries -> [entry | entries] end)}
+  end
+
+  defp list_entries do
+    Engine.list_entries()
+  end
+end
--- test/grantland/engine_test.exs
+++ test/grantland/engine_test.exs
@@ -61,4 +61,65 @@ defmodule Grantland.EngineTest do
       assert %Ecto.Changeset{} = Engine.change_pool(pool)
     end
   end
+
+  describe "entries" do
+    alias Grantland.Engine.Entry
+
+    @valid_attrs %{name: "some name", round: 42}
+    @update_attrs %{name: "some updated name", round: 43}
+    @invalid_attrs %{name: nil, round: nil}
+
+    def entry_fixture(attrs \\ %{}) do
+      {:ok, entry} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Engine.create_entry()
+
+      entry
+    end
+
+    test "list_entries/0 returns all entries" do
+      entry = entry_fixture()
+      assert Engine.list_entries() == [entry]
+    end
+
+    test "get_entry!/1 returns the entry with given id" do
+      entry = entry_fixture()
+      assert Engine.get_entry!(entry.id) == entry
+    end
+
+    test "create_entry/1 with valid data creates a entry" do
+      assert {:ok, %Entry{} = entry} = Engine.create_entry(@valid_attrs)
+      assert entry.name == "some name"
+      assert entry.round == 42
+    end
+
+    test "create_entry/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Engine.create_entry(@invalid_attrs)
+    end
+
+    test "update_entry/2 with valid data updates the entry" do
+      entry = entry_fixture()
+      assert {:ok, %Entry{} = entry} = Engine.update_entry(entry, @update_attrs)
+      assert entry.name == "some updated name"
+      assert entry.round == 43
+    end
+
+    test "update_entry/2 with invalid data returns error changeset" do
+      entry = entry_fixture()
+      assert {:error, %Ecto.Changeset{}} = Engine.update_entry(entry, @invalid_attrs)
+      assert entry == Engine.get_entry!(entry.id)
+    end
+
+    test "delete_entry/1 deletes the entry" do
+      entry = entry_fixture()
+      assert {:ok, %Entry{}} = Engine.delete_entry(entry)
+      assert_raise Ecto.NoResultsError, fn -> Engine.get_entry!(entry.id) end
+    end
+
+    test "change_entry/1 returns a entry changeset" do
+      entry = entry_fixture()
+      assert %Ecto.Changeset{} = Engine.change_entry(entry)
+    end
+  end
 end
